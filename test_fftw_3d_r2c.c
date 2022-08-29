2637099148a2eab6b34273f5f41803bca2512324
Alex Pletzer <alexander@gokliya.net>
2022-08-29 19:46:21 +0000

    reduced precision

--- test_fftw_3d_r2c.c
+++ test_fftw_3d_r2c.c
@@ -1,7 +1,7 @@
 /* Example showing the use of FFTW. 
 
 ml FFTW
-g++ -O4 test_fftw_3d_r2c.c -o test_fftw_3d_r2c -L $EBROOTFFTW/lib -lfftw3
+c
 
 */
 
@@ -15,8 +15,8 @@ g++ -O4 test_fftw_3d_r2c.c -o test_fftw_3d_r2c -L $EBROOTFFTW/lib -lfftw3
 // includes, project
 #include <fftw3.h>
 
-typedef fftw_complex Complex;
-typedef double Real;
+typedef fftwf_complex Complex;
+typedef float Real;
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -50,13 +50,13 @@ void runTest(int argc, char** argv)
     int mem_size2 = ntot2 * sizeof(Complex);
 
     // Allocate host memory for the signal
-    Real* h_signal = (Real*) fftw_malloc(mem_size);
-    Complex* d_signal = (Complex*) fftw_malloc(mem_size2);
-    Real* h_signal2 = (Real*) fftw_malloc(mem_size);    
+    Real* h_signal = (Real*) fftwf_malloc(mem_size);
+    Complex* d_signal = (Complex*) fftwf_malloc(mem_size2);
+    Real* h_signal2 = (Real*) fftwf_malloc(mem_size);    
 
     // FFTW plan
-    fftw_plan p = fftw_plan_dft_r2c_3d(SIGNAL_SIZE, SIGNAL_SIZE, SIGNAL_SIZE, h_signal, d_signal, FFTW_ESTIMATE);
-    fftw_plan p2 = fftw_plan_dft_c2r_3d(SIGNAL_SIZE, SIGNAL_SIZE, SIGNAL_SIZE, d_signal, h_signal2, FFTW_ESTIMATE);
+    fftwf_plan p = fftwf_plan_dft_r2c_3d(SIGNAL_SIZE, SIGNAL_SIZE, SIGNAL_SIZE, h_signal, d_signal, FFTW_ESTIMATE);
+    fftwf_plan p2 = fftwf_plan_dft_c2r_3d(SIGNAL_SIZE, SIGNAL_SIZE, SIGNAL_SIZE, d_signal, h_signal2, FFTW_ESTIMATE);
     
     // Initalize the memory for the signal
     for (unsigned int i = 0; i < ntot; ++i) {
@@ -66,26 +66,31 @@ void runTest(int argc, char** argv)
 
     clock_t time_beg = clock();
     
-    fftw_execute(p);
-    fftw_execute(p2);
-    
+    fftwf_execute(p);    
+    fftwf_execute(p2);
+
+    // Normalize
+    for (unsigned int i = 0; i < ntot; ++i) {
+        h_signal2[i] /= ntot;
+    }
+
     clock_t time_end = clock();
     printf("fwd -> bwd transform time: %lf secs\n", ((double)(time_end - time_beg))/CLOCKS_PER_SEC); 
         
     // Check
     float error = 0;
     for (unsigned int i = 0; i < ntot; ++i) {
-        error += h_signal[i] - h_signal2[i]/ntot; // note: normalization
+        error += h_signal[i] - h_signal2[i];
     }
     printf("error: %g\n", error);
 
     //Destroy CUFFT context
-    fftw_destroy_plan(p2);
-    fftw_destroy_plan(p);
+    fftwf_destroy_plan(p2);
+    fftwf_destroy_plan(p);
 
     // cleanup memory
-    fftw_free(h_signal);
-    fftw_free(d_signal);
-    fftw_free(h_signal2);
+    fftwf_free(h_signal);
+    fftwf_free(d_signal);
+    fftwf_free(h_signal2);
 }
 
