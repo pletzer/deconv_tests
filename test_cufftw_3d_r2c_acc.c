dc862436287649a5958384118ef3c6b62ca87305
Alex Pletzer <alexander@gokliya.net>
2022-09-01 05:34:18 +0000

    working on a OpenACC version

--- test_cufftw_3d_r2c_acc.c
+++ test_cufftw_3d_r2c_acc.c
@@ -1,7 +1,7 @@
 /* Example showing the use of FFTW. 
 
 ml shared NVHPC cuda11.0/fft
-pgcc -acc -cuda -O4 test_cufftw_3d_r2c.c -o test_cufftw_3d_r2c -L $EBROOTCUDA/lib -lcufftw -lcufft -lculibos
+pgcc -acc -cuda -O2 -Minfo=acc -Minfo=all test_cufftw_3d_r2c_acc.c -o test_cufftw_3d_r2c_acc -L $EBROOTCUDA/lib -lcufftw -lcufft -lculibos
 srun --gpus-per-node=A100:1 ./test_cufftw_3d_r2c
 
 */
@@ -59,8 +59,9 @@ void runTest(int argc, char** argv)
     for (unsigned int i = 0; i < ntot; ++i) {
         h_signal[i] = 0;
     }
-    h_signal[0] = 1;    
-
+    h_signal[0] = 1;
+    
+    
     // Data on the device
     Real* d_signal;
     cudaMalloc((void**)&d_signal, mem_size);
@@ -73,39 +74,41 @@ void runTest(int argc, char** argv)
     fftwf_plan p2 = fftwf_plan_dft_c2r_3d(SIGNAL_SIZE, SIGNAL_SIZE, SIGNAL_SIZE, c_signal, d_signal, FFTW_ESTIMATE);
 
     clock_t time_beg = clock();
-
-    // Copy host memory to device
-    cudaMemcpy(d_signal, h_signal, mem_size,
-                cudaMemcpyHostToDevice);
-        
-    fftwf_execute(p);
-    fftwf_execute(p2);
-    
-    #pragma acc host_data use_device(d_signal)
+    #pragma acc data deviceptr(c_signal[ntot2], d_signal[ntot])
     {
-        #pragma acc parallel loop copyin(ntot)
+
+        // Copy host memory to device
+        cudaMemcpy(d_signal, h_signal, mem_size,
+                    cudaMemcpyHostToDevice);
+
+        fftwf_execute(p);
+        fftwf_execute(p2);
+
+        #pragma acc parallel loop present(d_signal[ntot])
+        // Normalization
         for (unsigned int i = 0; i < ntot; ++i) {
             d_signal[i] /= ntot;
         }
+
+        // Copy device memory to host
+        cudaMemcpy(h_signal2, d_signal, mem_size,
+                   cudaMemcpyDeviceToHost);
+
     }
-    
-    // Copy device memory to host
-    cudaMemcpy(h_signal2, d_signal, mem_size,
-               cudaMemcpyDeviceToHost);
     
     clock_t time_end = clock();
     printf("fwd -> bwd transform time: %lf secs\n", ((double)(time_end - time_beg))/CLOCKS_PER_SEC); 
-    
+
+    // Destroy plans
+    fftwf_destroy_plan(p2);
+    fftwf_destroy_plan(p);
+
     // Check
     float error = 0;
     for (unsigned int i = 0; i < ntot; ++i) {
         error += h_signal[i] - h_signal2[i];
     }
     printf("error: %g\n", error);
-
-    //Destroy plans
-    fftwf_destroy_plan(p2);
-    fftwf_destroy_plan(p);
 
     // cleanup memory
     cudaFree(d_signal);
